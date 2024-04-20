dc594d3f735aeb421698ec697b512702c5236913
andrrew24 <91123740+andrrew24@users.noreply.github.com>
2024-04-20 18:00:16 +0200

    biesecion done

--- lib/utils/constants/app_colors.dart
+++ lib/utils/constants/app_colors.dart
@@ -5,4 +5,4 @@ const primarybtnColor = Color(0xffFA7921);
 const backbtnColor = Color(0xff255C99);
 const resetbtnColor = Color(0xff992525);
 const solvebtnColor = Color(0xff3D98FD);
-const txtfeldColor = Color(0xffD9D9D9);
+const txtfeldTableColor = Color(0xffD9D9D9);
--- lib/views/forms/polynomials/biesection_form.dart
+++ lib/views/forms/polynomials/biesection_form.dart
@@ -1,7 +1,6 @@
-import 'dart:ffi';
-
+import 'package:awesome_snackbar_content/awesome_snackbar_content.dart';
+import 'package:data_table_2/data_table_2.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:gap/gap.dart';
 import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -33,6 +32,58 @@ class _BiesectionFormState extends State<BiesectionForm> {
   late double xu;
   late String eq;
 
+  static List<String> tableheader = [
+    "Iter",
+    "Xl",
+    "f(Xl)",
+    "Xu",
+    "f(Xu)",
+    "Xr",
+    "f(Xr)",
+    "Error%"
+  ];
+
+  reset() {
+    setState(() {
+      biesection.iterations = [];
+      xlcontroller.clear();
+      xucontroller.clear();
+      errorcontroller.clear();
+      eqcontroller.clear();
+      isAcive = false;
+    });
+  }
+
+  solve() {
+    setState(() {
+      try {
+        error = double.parse(errorcontroller.text);
+        xl = double.parse(xlcontroller.text);
+        xu = double.parse(xucontroller.text);
+        eq = eqcontroller.text;
+        biesection =
+            Biesection(eps: error, xL: xl, xU: xu, expressionString: eq);
+        biesection.biesection();
+        isAcive = true;
+      } catch (e) {
+        reset();
+        final snackBar = SnackBar(
+          duration: const Duration(seconds: 2),
+                            clipBehavior: Clip.none,
+                            elevation: 0,
+                            behavior: SnackBarBehavior.fixed,
+                            backgroundColor: Colors.transparent,
+                            content: AwesomeSnackbarContent(
+                              title: "Error",
+                              message: "Enter Valid Input",
+                              contentType: ContentType.warning,
+                            ));
+                        ScaffoldMessenger.of(context).hideCurrentSnackBar();
+                        ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MethodForm(
@@ -41,30 +92,10 @@ class _BiesectionFormState extends State<BiesectionForm> {
           GoRouter.of(context).go('/');
         },
         resetOnpressed: () {
-          setState(() {
-            biesection.iterations = [];
-            xlcontroller.clear();
-            xucontroller.clear();
-            errorcontroller.clear();
-            eqcontroller.clear();
-            isAcive = false;
-          });
+          reset();
         },
         solveOnpressed: () {
-          setState(() {
-            try {
-              error = double.parse(errorcontroller.text);
-              xl = double.parse(xlcontroller.text);
-              xu = double.parse(xucontroller.text);
-              eq = eqcontroller.text;
-              biesection =
-                  Biesection(eps: error, xL: xl, xU: xu, expressionString: eq);
-              biesection.biesection();
-              isAcive = true;
-            } catch (e) {
-              print(e);
-            }
-          });
+          solve();
         },
         content: Column(
           children: [
@@ -76,19 +107,19 @@ class _BiesectionFormState extends State<BiesectionForm> {
                   controller: eqcontroller,
                   width: 500,
                 ),
-                Gap(103),
+                const Gap(103),
                 CustomInputField(
                   label: "Xl",
                   controller: xlcontroller,
                   width: 90,
                 ),
-                Gap(103),
+                const Gap(103),
                 CustomInputField(
                   label: "Xu",
                   controller: xucontroller,
                   width: 90,
                 ),
-                Gap(103),
+                const Gap(103),
                 CustomInputField(
                   label: "E%",
                   controller: errorcontroller,
@@ -99,35 +130,60 @@ class _BiesectionFormState extends State<BiesectionForm> {
             const Gap(30),
             isAcive
                 ? Container(
-                    color: Colors.white,
-                    child: DataTable(
-                      border: TableBorder.all(color: Colors.white),
-                      columns: const [
-                        DataColumn(label: Text("iter")),
-                        DataColumn(label: Text("Xl")),
-                        DataColumn(label: Text("f(Xl)")),
-                        DataColumn(label: Text("Xu")),
-                        DataColumn(label: Text("f(Xu)")),
-                        DataColumn(label: Text("Xr")),
-                        DataColumn(label: Text("f(Xr)")),
-                        DataColumn(label: Text("error")),
-                      ],
+                    decoration: ShapeDecoration(
+                        color: txtfeldTableColor,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    height: MediaQuery.sizeOf(context).height * 0.6,
+                    child: DataTable2(
+                      scrollController: ScrollController(),
+                      columns: tableheader
+                          .map((e) => DataColumn(
+                                  label: Text(
+                                e,
+                                style: GoogleFonts.montserrat(
+                                    textStyle: const TextStyle(fontSize: 15)),
+                              )))
+                          .toList(),
                       rows: biesection.iterations
                           .map((e) => DataRow(cells: [
-                                DataCell(Text(e.iter.toStringAsFixed(0))),
-                                DataCell(Text(e.xl.toStringAsFixed(3))),
-                                DataCell(Text(e.fxl.toStringAsFixed(3))),
-                                DataCell(Text(e.xu.toStringAsFixed(3))),
-                                DataCell(Text(e.fxu.toStringAsFixed(3))),
-                                DataCell(Text(e.xr.toStringAsFixed(3))),
-                                DataCell(Text(e.fxr.toStringAsFixed(3))),
-                                DataCell(
-                                    Text("${e.error.toStringAsFixed(2)} %")),
+                                DataCell(Text(e.iter.toStringAsFixed(0),
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
+                                DataCell(Text(e.xl.toStringAsFixed(3),
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
+                                DataCell(Text(e.fxl.toStringAsFixed(3),
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
+                                DataCell(Text(e.xu.toStringAsFixed(3),
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
+                                DataCell(Text(e.fxu.toStringAsFixed(3),
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
+                                DataCell(Text(e.xr.toStringAsFixed(3),
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
+                                DataCell(Text(e.fxr.toStringAsFixed(3),
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
+                                DataCell(Text("${e.error.toStringAsFixed(2)} %",
+                                    style: GoogleFonts.montserrat(
+                                        textStyle:
+                                            const TextStyle(fontSize: 15)))),
                               ]))
                           .toList(),
                     ),
                   )
-                : SizedBox(),
+                : const SizedBox(),
           ],
         ));
   }
@@ -161,10 +217,12 @@ class CustomInputField extends StatelessWidget {
         SizedBox(
           width: width,
           child: TextField(
+            style: GoogleFonts.montserrat(
+                textStyle: const TextStyle(fontSize: 15)),
             controller: controller,
-            decoration: InputDecoration(
+            decoration: const InputDecoration(
                 filled: true,
-                fillColor: txtfeldColor,
+                fillColor: txtfeldTableColor,
                 border: OutlineInputBorder(
                     borderRadius: BorderRadius.all(Radius.circular(15)))),
           ),
@@ -189,3 +247,33 @@ class CustomInputField extends StatelessWidget {
 
 // Biesection(
 //       eps: 1, xL: 0, xU: 1, expressionString: "4 * x^3 - 6 * x^2 + 7*x - 2.3");
+
+
+
+//working table
+// DataTable(
+//                         border: TableBorder.all(color: Colors.white),
+//                         columns: const [
+//                           DataColumn(label: Text("iter")),
+//                           DataColumn(label: Text("Xl")),
+//                           DataColumn(label: Text("f(Xl)")),
+//                           DataColumn(label: Text("Xu")),
+//                           DataColumn(label: Text("f(Xu)")),
+//                           DataColumn(label: Text("Xr")),
+//                           DataColumn(label: Text("f(Xr)")),
+//                           DataColumn(label: Text("error")),
+//                         ],
+//                         rows: biesection.iterations
+//                             .map((e) => DataRow(cells: [
+//                                   DataCell(Text(e.iter.toStringAsFixed(0))),
+//                                   DataCell(Text(e.xl.toStringAsFixed(3))),
+//                                   DataCell(Text(e.fxl.toStringAsFixed(3))),
+//                                   DataCell(Text(e.xu.toStringAsFixed(3))),
+//                                   DataCell(Text(e.fxu.toStringAsFixed(3))),
+//                                   DataCell(Text(e.xr.toStringAsFixed(3))),
+//                                   DataCell(Text(e.fxr.toStringAsFixed(3))),
+//                                   DataCell(
+//                                       Text("${e.error.toStringAsFixed(2)} %")),
+//                                 ]))
+//                             .toList(),
+//                       ),
